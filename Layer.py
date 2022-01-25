dab8707fa1e2c46cc9ffde7b0a3e801122b29ffd
Tom <tccatalan7@gmail.com>
2022-01-24 21:17:47 -0800

    xavier initialization n that

--- Layer.py
+++ Layer.py
@@ -48,7 +48,10 @@ class FullyConnected():
         else:
             self.shape=s
         if w==[]:
-            self.weights=np.random.randn(self.shape[1], self.shape[0])
+            #scale = 1/max(1., sum(self.shape)/2.)
+            #limit = np.sqrt(3.0 * scale)
+            limit = np.sqrt(6/sum(self.shape))
+            self.weights = np.random.uniform(low=-limit, high=limit, size=(self.shape[1], self.shape[0]))
         else:
             self.weights=w
         if b==[]:
--- network.py
+++ network.py
@@ -2,6 +2,7 @@ from Layer import *
 from images import test_normal
 import numpy as np
 import json
+import time
 
 e=0.0000001
 
@@ -40,10 +41,13 @@ class cnn():
             grad = layer.backpropagate(grad, self.learning_rate)
 
     def train(self, train_data, show_progress=False):
+        time_start = time.time()
         for train_index in range(len(train_data)):
             self.backpropagate(train_data[train_index][0], train_data[train_index][1])
             if show_progress:
                 print("Training: {}/{}".format(train_index, len(train_data)))
+        time_taken = time.time()-time_start
+        print("{} minutes, {} seconds".format(time_taken//60, time_taken%60))
 
     def test(self, test_data, debug=False):
         accuracy, avg_loss = 0, 0
@@ -91,12 +95,13 @@ from tensorflow.keras.datasets.mnist import load_data
 mnist = load_data()
 training, testing = [(i/254, l) for i, l in zip(mnist[0][0], mnist[0][1])], [(i/254, l) for i, l in zip(mnist[1][0], mnist[1][1])]
 if __name__ == "__main__":
-    test_size = 100
-    test_net = cnn("test", [convolutionLayer(3, [3, 3]), poolingLayer("MAX", [2, 2]), ReLu(), FullyConnected(s=[13*13*3, 10]), Softmax()], 0.06)
-    test_net.train(training[:1000], show_progress=True)
-    test_net.serialize()
+    test_size = 1000
+    num_filters=3
+    #test_net = cnn("test", [convolutionLayer(num_filters, [3, 3]), poolingLayer("MAX", [2, 2]), ReLu(), FullyConnected(s=[13*13*num_filters, 10]), Softmax()], 0.06)
+    #test_net.train(training[:10000], show_progress=True)
+    #test_net.serialize()
 
-    #test_net=load_network("test")
+    test_net=load_network("test")
     test_net.test(training[:test_size])
 
 
